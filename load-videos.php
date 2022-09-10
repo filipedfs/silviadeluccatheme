f5a21f386e7b93e715f4c4367eee83b3e50a2f1e
LuizaAlanis <luizalanisantos@gmail.com>
2022-09-10 18:18:27 -0300

    Studying new API request

--- load-videos.php
+++ load-videos.php
@@ -3,9 +3,13 @@
 function load_videos($channel_id, $max_results){
     // api config 
     $api_key = 'AIzaSyDeNytuiXpp29udQbwjTyukRO8J-FKdtrU';
-
+    $playlist_id = 'PLkKbqsoc9ce9uoRlEV8jZE5sGqa33PMPF';
     try{
         $api_data = @file_get_contents('https://www.googleapis.com/youtube/v3/search?order=date&part=snippet&channelId='.$channel_id.'&maxResults='.$max_results.'&key='.$api_key.''); 
+        
+        // TODO: Fix request to bring videos from playlist
+        // $api_data = @file_get_contents('https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId='.$playlist_id.'&key='.$api_key.'');
+        
         if($api_data)
         $video_list = json_decode($api_data);
         else
