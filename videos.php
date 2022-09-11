9a5f277f01d87e9dd487fa2cf863436696120c5c
LuizaAlanis <luizalanisantos@gmail.com>
2022-09-11 02:21:33 -0300

    Final adjustments

--- videos.php
+++ videos.php
@@ -81,6 +81,28 @@
                 </p>
             </div>
             <div class="iframe-container-basic">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/WZLue-9UJEU" title="YouTube video player" frameborder="0" allow="
+                        accelerometer; 
+                        autoplay; 
+                        clipboard-write; 
+                        encrypted-media; 
+                        gyroscope; 
+                        picture-in-picture" allowfullscreen></iframe>
+            </div>
+        </div>
+        <div class="row-2-columns-basic">
+            <div>
+                <b class="subtitle subtitle-small">
+                    "Silvia de Lucca: Série Compositores Brasileiros"
+                    pelo Movimento Uirapuru
+                    (2020)
+                </b>
+                <br><br><br>
+                <p class="basic-text">
+                    Silvia de Lucca concede entrevista via internet, em 21 de setembro de 2020, ao Movimento Uirapuru.
+                </p>
+            </div>
+            <div class="iframe-container-basic">
                 <iframe width="560" height="315" src="https://www.youtube.com/embed/v4RTeZD-_6U" title="YouTube video player" frameborder="0" allow="
                         accelerometer; 
                         autoplay; 
@@ -93,24 +115,16 @@
         <div class="row-2-columns-basic">
             <div>
                 <b class="subtitle subtitle-small">
-                    Sun d'Oro Suite
-                    de Silvia de Lucca
-                    no Estudio Salaviva - Espaço Cachuera! (SP)
-                    (2021)
+                    Sobre "Percepção", um termo ténico-musical
+                    por Silvia de Lucca
+                    (2020)
                 </b>
                 <br><br><br>
-                <ul class="basic-ul">
-                    <li>Composição de Silvia de Lucca em gravação,</li>
-                    <li>Estudio Salaviva - Espaço Cachuera! (SP) - 12.02.2021</li>
-                    <li><br></li>
-                    <li class="li-bolder">Intérpretes:</li>
-                    <li>Regência: Ricardo Bologna</li>
-                    <li>Violinos I: Luiz Amato (spalla), Caio Paiva dos Santos, Guilherme Oliveira, Everton de Novaes</li>
-                    <li>Violinos II: Matthew Thorpe, Jonathan Souza Cardoso dos Santos, Nádia Fonseca da Silva, Marina Lima Rodrigues Dias</li>
-                    <li>Violas: Ederson Fernandes, João Senna, Yohanna Alves</li>
-                    <li>Violoncelos: Adriana Holtz, Santo Scala</li>
-                    <li>Contrabaixos: Alexandre Rosa, Davi Ciriaco</li>
-                </ul>
+                <p class="basic-text">
+                    Silvia de Lucca fala sobre o termo técnico-musical Percepção, a convite do Coletivo Tranças
+                    (grupo de percussionistas vinculado ao grupo PIAP-UNESP), para integrar o EnconTRA-Encontro
+                    Musical Online, de 9 à 12 de outubro de 2020.
+                </p>
             </div>
             <div class="iframe-container-basic">
                 <iframe width="560" height="315" src="https://www.youtube.com/embed/01luQo_9l-Y" title="YouTube video player" frameborder="0" allow="
@@ -125,26 +139,171 @@
         <div class="row-2-columns-basic">
             <div>
                 <b class="subtitle subtitle-small">
-                    "Silvia de Lucca: Série Compositores Brasileiros"
-                    pelo Movimento Uirapuru
-                    (2020)+
+                    Em Memória
+                    de Silvia de Lucca
+                    na Sala São Paulo
+                    (2019)
                 </b>
                 <br><br><br>
-                <ul class="basic-ul">
-                    <li>Composição de Silvia de Lucca em gravação,</li>
-                    <li>Estudio Salaviva - Espaço Cachuera! (SP) - 12.02.2021</li>
-                    <li><br></li>
-                    <li class="li-bolder">Intérpretes:</li>
-                    <li>Regência: Ricardo Bologna</li>
-                    <li>Violinos I: Luiz Amato (spalla), Caio Paiva dos Santos, Guilherme Oliveira, Everton de Novaes</li>
-                    <li>Violinos II: Matthew Thorpe, Jonathan Souza Cardoso dos Santos, Nádia Fonseca da Silva, Marina Lima Rodrigues Dias</li>
-                    <li>Violas: Ederson Fernandes, João Senna, Yohanna Alves</li>
-                    <li>Violoncelos: Adriana Holtz, Santo Scala</li>
-                    <li>Contrabaixos: Alexandre Rosa, Davi Ciriaco</li>
-                </ul>
+                <p class="basic-text">
+                    Orquestra Sinfônica da Universidade de São Paulo<br>
+                    Temporada oficial<br>
+                    Sala São Paulo - 02.11.2019
+                    <br><br>
+                    Regência: Catherine Larsen-Maguire
+                </p>
             </div>
             <div class="iframe-container-basic">
                 <iframe width="560" height="315" src="https://www.youtube.com/embed/i9-WIQf0gHw" title="YouTube video player" frameborder="0" allow="
+                        accelerometer; 
+                        autoplay; 
+                        clipboard-write; 
+                        encrypted-media; 
+                        gyroscope; 
+                        picture-in-picture" allowfullscreen></iframe>
+            </div>
+        </div>
+        <div class="row-2-columns-basic">
+            <div>
+                <b class="subtitle subtitle-small">
+                    Cordas Sapecas
+                    de Silvia de Lucca
+                    no Vernon Filley Art Museum
+                    (2019)
+                </b>
+                <br><br><br>
+                <p class="basic-text">
+                    Composição de Silvia de Lucca no concerto "Cello Virtuoso, Ji Yon Shim
+                    Anderson", em fevereiro de 2019, no Vernon Filley Art Museum, em Pratt / Kansas
+                    (USA).
+                    <br><br>
+                    Intérprete: Ji Yon Shim (violoncelo)
+                </p>
+            </div>
+            <div class="iframe-container-basic">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/QANx0ifPkx4" title="YouTube video player" frameborder="0" allow="
+                        accelerometer; 
+                        autoplay; 
+                        clipboard-write; 
+                        encrypted-media; 
+                        gyroscope; 
+                        picture-in-picture" allowfullscreen></iframe>
+            </div>
+        </div>
+        <div class="row-2-columns-basic">
+            <div>
+                <b class="subtitle subtitle-small">
+                    "Estética, Criação, Poética e Feminino"
+                    com Silvia de Lucca
+                    no Unibes Cultural
+                    (2016)
+                </b>
+                <br><br><br>
+                <p class="basic-text">
+                    Silvia de Lucca participa do encontro ao vivo, em outubro de 2016, no Unibes Cultural, em São Paulo/SP.
+                    Mediação e curadoria de Sonia Sobral, com a presença também de Tata Amaral e Maris Thais.
+                </p>
+            </div>
+            <div class="iframe-container-basic">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/K9SnBXeyeNk" title="YouTube video player" frameborder="0" allow="
+                        accelerometer; 
+                        autoplay; 
+                        clipboard-write; 
+                        encrypted-media; 
+                        gyroscope; 
+                        picture-in-picture" allowfullscreen></iframe>
+            </div>
+        </div>
+        <div class="row-2-columns-basic">
+            <div>
+                <b class="subtitle subtitle-small">
+                    Quinteto Puelli
+                    de Silvia de Lucca
+                    no Centro Cultural São Paulo
+                    (2014)
+                </b>
+                <br><br><br>
+                <p class="basic-text">
+                    "Silvia de Lucca: um tributo ao contrabaixo"
+                    Centro Cultural São Paulo - 30.09.2014
+                    <br><br>
+                    Intérpretes: Luis Amato (violino), Anderson Fernandes (viola), Adriana Holtz (violoncelo), Alexandre Rosa (contrabaixo), Karin Fernandes (piano)
+                </p>
+            </div>
+            <div class="iframe-container-basic">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/b9SRnqChFC4" title="YouTube video player" frameborder="0" allow="
+                        accelerometer; 
+                        autoplay; 
+                        clipboard-write; 
+                        encrypted-media; 
+                        gyroscope; 
+                        picture-in-picture" allowfullscreen></iframe>
+            </div>
+        </div>
+        <div class="row-2-columns-basic">
+            <div>
+                <b class="subtitle subtitle-small">
+                    Curso Eu Adoro Música
+                    por Silvia de Lucca
+                    (2011)
+                </b>
+                <br><br><br>
+                <p class="basic-text">
+                    Silvia de Lucca fala sobre seu curso "Eu adoro música, mas não entendo nada!" ministrado desde 1992 em diferentes partes do país
+                </p>
+            </div>
+            <div class="iframe-container-basic">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/myKsjVVWQ3M" title="YouTube video player" frameborder="0" allow="
+                        accelerometer; 
+                        autoplay; 
+                        clipboard-write; 
+                        encrypted-media; 
+                        gyroscope; 
+                        picture-in-picture" allowfullscreen></iframe>
+            </div>
+        </div>
+        <div class="row-2-columns-basic">
+            <div>
+                <b class="subtitle subtitle-small">
+                    A "Fórmula do Sucesso" e o Ouvinte de Rádio
+                    com Silvia de Lucca
+                    no Itaú Cultural
+                    (2008)
+                </b>
+                <br><br><br>
+                <p class="basic-text">
+                    Silvia de Lucca participa do programa Redes da Criação, gravado em junho de 2008, no Itaú Cultural,
+                    em São Paulo/SP. Com mediação de Claudiney Ferreira e a participação do músico Sérgio Nader.
+                </p>
+            </div>
+            <div class="iframe-container-basic">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/6kdYLmMRcfk" title="YouTube video player" frameborder="0" allow="
+                        accelerometer; 
+                        autoplay; 
+                        clipboard-write; 
+                        encrypted-media; 
+                        gyroscope; 
+                        picture-in-picture" allowfullscreen></iframe>
+            </div>
+        </div>
+        <div class="row-2-columns-basic">
+            <div>
+                <b class="subtitle subtitle-small">
+                    Três Correntes de Outono
+                    de Silvia de Lucca
+                    no Espaço Cultural CPFL
+                    (2007)
+                </b>
+                <br><br><br>
+                <p class="basic-text">
+                    Silvia de Lucca comenta o processo composicional de sua obra Três Correntes de Outono, para orquestra de cordas,
+                    encomendada para o projeto "Quatro estações para o século XXI (O Outono do Século XXI)", estreada em 10 de novembro
+                    de 2007, no Espaço Cultural CPFL, em Campinas/SP.<br>
+                    Curadoria e regência: Henrique Lian
+                </p>
+            </div>
+            <div class="iframe-container-basic">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/D-BMFjQkFmM" title="YouTube video player" frameborder="0" allow="
                         accelerometer; 
                         autoplay; 
                         clipboard-write; 
