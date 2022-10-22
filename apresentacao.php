efeadba5c85eadd48cb752187d413a78581c9346
Filipe Fonseca <filipedfs@gmail.com>
2022-10-22 09:35:56 -0300

    Update apresentacao

--- apresentacao.php
+++ apresentacao.php
@@ -25,60 +25,51 @@
 <div class="main-content">
   <h3 class="subtitle">Apresentação</h3>
   <hr />
-  <p class="basic-text">[Em processo de atualização]</p>
   <div class="row-2-columns">
     <div>
       <p class="first-stanza">
         <span class="first-letter">C</span>
         <span class="basic-text">ompositora especializada nos conservatórios de Zurique e Genebra entre 1989 e 1993.</span>
       </p>
-      <p>
-        É mestre em artes pela ECA-USP. Possui múltiplas experiências musicais também como pianista,
-        camerista, madrigalista, violista, preparadora coral e membro de orquestras jovens.
-      </p>
-      <p>
-        Na Composição foi orientada no Brasil por Schnorrenberg, Kelly, Santoro, Ficarelli e Escobar e na
-        Suíça por Lehmann e Balissat.
-      </p>
-      <p>
-        Suas obras tem sido apresentadas regularmente, além de receber constantes solicitações e encomendas
-        de novas composições, no país e no exterior.
-        Em Zurique (1990) e em São Paulo (1996, 2010 e 2014) foram realizados concertos totalmente dedicados
-        às suas obras.
-      </p>
-      <p>
-        Seu catálogo de composições conta com obras para diferentes formações instrumentais: solos, câmara,
-        orquestra de cordas e sinfônica.
-      </p>
-      <p>
-        Profissional autônoma, atuou repetidamente como membro de júri em vários concursos, como o Concurso
-        Nacional de Composição Camargo Guarnieri, Projeto Nascente, Prêmio Carlos Gomes, Prelúdio, ProAC -
-        Música Erudita e Carnaval São Paulo.
-      </p>
-      <p>
-        Atualmente dedica-se à Composição, ao seu projeto “Eu adoro música, mas não entendo nada!” e ao
-        ensino em algumas disciplinas musicais, além de realizar e orientar pesquisas, dar palestras,
-        entrevistas, publicar artigos, idealizar e coordenar projetos sócio-educativo-culturais, etc.
-      </p>
+      <p>É mestre em artes pela ECA-Universidade de São Paulo desde 2002. Possui múltiplas experiências musicais também como pianista, camerista, violista,
+         membro de orquestras jovens, madrigalista e preparadora coral.</p>
+      <p>Na Composição foi orientada no Brasil por Schnorrenberg, Kelly, Santoro, Ficarelli e Escobar e na Suíça por Lehmann e Balissat.</p>
+      <p>Suas obras têm sido apresentadas regularmente, além de receber constantes solicitações e encomendas de novas composições, no país e no exterior. Em
+         Zurique (1990) e em São Paulo (1996, 2010 e 2014) foram realizados concertos totalmente dedicados às suas obras.</p>
+      <p>Seu catálogo de composições conta com obras para diferentes formações instrumentais: solos, câmara, orquestra de cordas e sinfônica.</p>
+      <p>Profissional autônoma por opção, atuou repetidamente como membro de júri em vários concursos de composição e interpretação, como o Concurso Nacional de
+         Composição Camargo Guarnieri, Projeto Nascente, Prêmio Carlos Gomes, Prelúdio, ProAC - Música Erudita e Carnaval São Paulo.</p>
+      <p>Atualmente dedica-se à Composição, ao seu projeto “Eu adoro música, mas não entendo nada!” destinado ao grande público e ao ensino em algumas
+         disciplinas musicais, além de realizar e orientar pesquisas, publicar artigos, idealizar e coordenar projetos sócio-educativo-musicais, dar palestras
+         etc.</p>
     </div>
     <div>
-      <h3 class="subtitle subtitle-small">EVENTOS</h3>
+      <h3 class="subtitle subtitle-small">Presença em Eventos</h3>
       <ul>
-        <li>EnconTRA-Encontro Musical Online (via internet);</li>
-        <li>Movimento Uirapuru - série de compositores brasileiros (via internet);</li>
-        <li>Seminário A Mulher na Música - Rio de Janeiro;</li>
-        <li>1ª Semana da Música FIAM-FAAM - São Paulo;</li>
-        <li>Festivais Música Nova - São Paulo, Ribeirão Preto e Santos;</li>
-        <li>Bienais de Música Brasileira Contemporânea - Rio de Janeiro;</li>
-        <li>Encontro de Compositores Latino-Americano - Porto Alegre;</li>
-        <li>Festival SG-2003 - Suíça;</li>
-        <li>Encontro Latino-Americano de Compositores e Intérpretes - Belo Horizonte;</li>
-        <li>Juni-Festwochen Zürich - Suíça;</li>
-        <li>Festival de Música de Londrina;</li>
-        <li>Festivais Internacionais de Mulheres Compositoras - São Paulo e Poços de Caldas;</li>
-        <li>Festival de Inverno de Campos do Jordão.</li>
-      </uL>
-      </p>
+        <li>Entrevista - SOM na tELA – ELA TV – Escola Livre de Artes (evento on-line);</li>
+        <li>Entrevista - Série Especial: Compositoras e Compositores do Brasil - site Tribuna da Imprensa Livre – Jornalismo em defesa do Brasil!;</li>
+        <li>Entrevista - Você é Top - TV Relacionamento - São Paulo;</li>
+        <li>Compositora - 8º Festival de Música Erudita do Espírito Santo (evento on-line);</li>
+        <li>Mesa Redonda - EnconTRA - Encontro Musical Online (evento on-line);</li>
+        <li>Debate - Ingredientes e Identidade - Simpósio Identidade Brasileira na Música de Concerto (evento on-line);</li>
+        <li>Compositora - Projeto SINOS (Sistema Nacional de Orquestras Sociais do Brasil) - FUNARTE e Escola de Música da UFRJ – Rio de Janeiro;</li>
+        <li>Entrevista / conversa virtual - Movimento Uirapuru - série de compositores brasileiros (evento on-line);</li>
+        <li>Apresentação e Conversa com Crescidos – Música nos Gambozinos - Porto (Portugal);</li>
+        <li>Debate - A Atuação Feminina no Campo do Composição - Seminário A Mulher na Música - Rio de Janeiro;</li>
+        <li>Palestra - Processos Composicionais - 1ª Semana da Música FIAM-FAAM - São Paulo;</li>
+        <li>Entrevista - Música e Músicos do Brasil - Programa da Rádio MEC - Rio de Janeiro;</li>
+        <li>Entrevista - Compositoras Brasileiras - ClickTV – TV UOL - São Paulo;</li>
+        <li>Compositora - Festival de Inverno de Campos do Jordão;</li>
+        <li>Compositora - Festivais Música Nova - São Paulo, Ribeirão Preto e Santos;</li>
+        <li>Compositora - Bienais de Música Brasileira Contemporânea - Rio de Janeiro;</li>
+        <li>Entrevistas – Café Musical; Tema e Variações; Supertônica; Boulevar Cultura; Contraponto; Em Presença - Rádio Cultura FM de São Paulo;</li>
+        <li>Compositora - Festival SG-2003 - Suíça;</li>
+        <li>Compositora - Encontro de Compositores Latino-Americano - Porto Alegre;</li>
+        <li>Compositora - Encontro Latino-Americano de Compositores e Intérpretes - Belo Horizonte;</li>
+        <li>Compositora - Festivais Internacionais de Mulheres Compositoras - São Paulo e Poços de Caldas;</li>
+        <li>Docente - Festival de Música de Londrina;</li>
+        <li>Compositora - Juni-Festwochen Zürich – Suíça.</li>
+      </ul>
     </div>
   </div>
 </div>
